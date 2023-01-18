271e3e11ce709cf79ee8a829f58179aaa8f8d5c2
K.Dot <110929758+KDot227@users.noreply.github.com>
2023-01-18 15:51:40 -0600

    Update setup.bat
    batched

--- setup.bat
+++ setup.bat
@@ -1,6 +1,29 @@
 @echo off
+:: Enhanced by Godfather
+cd /d %~dp0
 color 5
 title Installing requirements...
+
+python --version > python.txt
+if %errorlevel% neq 0 (
+    echo Python is not installed or not added to path.
+    if exist "%localappdata%\Programs\Python\Python310\python.exe" (
+        echo Python is installed but not added to path.
+    )
+    pause
+    exit /b
+)
+
+python -m pip --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Pip is not installed or not added to path.
+    if exist "%localappdata%\Programs\Python\Python310\Scripts\pip.exe" (
+        echo Pip is installed but not added to path.
+    )
+    pause
+    exit /b
+)
+
 python -m pip install -r requirements.txt
 cls
 cd tools
