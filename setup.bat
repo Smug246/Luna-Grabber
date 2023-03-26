676eb21886a979521d0ecd71a1bce2d04613f811
unknown <bossboi281@gmail.com>
2023-03-26 18:00:25 +0100

    changed version

--- setup.bat
+++ setup.bat
@@ -3,16 +3,16 @@ color 5
 title Installing Dependencies...
 
 rem Check if Python 3.11.1 is installed
-python --version | findstr "3.11.1"
+python --version | findstr "3.11"
 if %errorlevel%==0 (
-    echo Python 3.11.1 is already installed
+    echo Python 3.11 is already installed
 ) else (
-    echo Python 3.11.1 is not installed. Downloading...
-    curl -L -o python-3.11.1.exe https://www.python.org/ftp/python/3.11.0/python-3.11.1-amd64.exe
-    echo Installing Python 3.11.1...
-    start /wait python-3.11.1.exe /quiet InstallAllUsers=1 Include_test=0
-    del python-3.11.1.exe
-    echo Python 3.11.1 has been installed
+    echo Python 3.11 is not installed. Downloading...
+    curl -L -o python-3.11.exe https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe
+    echo Installing Python 3.11...
+    start /wait python-3.11.exe /quiet InstallAllUsers=1 Include_test=0
+    del python-3.11.exe
+    echo Python 3.11 has been installed
 )
 
 rem Install requirements
