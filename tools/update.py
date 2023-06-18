3289495bada464d8f5d7eaaa9ac725952b300076
unknown <bossboi281@gmail.com>
2023-06-18 22:41:31 +0100

    big new update

--- tools/update.py
+++ tools/update.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from time import sleep
 from zipfile import ZipFile
 
@@ -7,18 +8,21 @@ import requests
 
 class Update():
     def __init__(self):
-        self.version = '1.5.5'
+        self.version = '1.5.6'
         self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/tools/update.py'
         self.zipfile = 'https://github.com/Smug246/Luna-Grabber/archive/refs/heads/main.zip'
         self.update_checker()
 
     def update_checker(self):
         code = requests.get(self.github).text
-        if "self.version = '1.5.5'" in code:
+        if "self.version = '1.5.6'" in code:
             print('This version is up to date!')
+            sleep(1)
+            print('You can now open run.bat to open the builder!')
+            sleep(1)
             print('Exiting...')
             sleep(2)
-            exit()
+            sys.exit()
         else:
             print('''
                     ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
@@ -42,7 +46,7 @@ class Update():
             if choice.lower() == 'n':
                 print('Exiting...')
                 sleep(2)
-                exit()
+                sys.exit()
 
 
 if __name__ == '__main__':
