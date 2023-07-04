06634e887d213f8e1e82b65aee02cebdc4955348
unknown <bossboi281@gmail.com>
2023-07-04 14:42:30 +0100

    fixed python checking

--- setup.bat
+++ setup.bat
@@ -2,13 +2,13 @@
 color 5
 
 title Checking Python Version
-python --version | findstr "3.11.2"
+python --version | findstr "3.11."
 if %errorlevel%==0 (
-    echo Python 3.11.2 is already installed
+    echo Python 3.11.0 or higher is already installed
 ) else (
-    echo Python 3.11.2 is not installed
-    echo Please install Python 3.11.2 and it it to your PATH
-    exit /b
+    echo Python 3.11.0 or higher is not installed
+    echo Please install Python 3.11.0 or higher and add it to your PATH
+    timeout /t 5 >nul
 )
 
 title Installing Requirements
